d59e880ab253ff34bd78183a88e9032703b0f970
Eric Chiu <eric840906@hotmail.com.tw>
2020-08-08 12:00:44 +0800

    first commit

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/VueProject1/',
     proxyTable: {},
 
     // Various Dev Server settings
